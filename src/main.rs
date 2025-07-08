bcddcc6708a16af40a2bc8e96c0ca527b1c9b219
Graham Lancaster <sailthesouth@gmail.com>
2025-07-08 00:44:35 -0400

    Feature: Added a view to show potential digits for a cell

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use eframe::egui;
-use reqwest::blocking::{get, Client};
-use serde::{Serialize, Deserialize};
-use std::sync::{Arc,Mutex};
+use reqwest::blocking::{Client, get};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
@@ -19,6 +19,8 @@ struct SudokuSolverApp {
     puzzle: [[usize; 9]; 9],
     solution: [[usize; 9]; 9],
     difficulty: String,
+    show_potentials: bool,
+    potentials: [[u16; 9]; 9],
 }
 
 impl SudokuSolverApp {
@@ -35,9 +37,13 @@ impl SudokuSolverApp {
             solution: true,
             array: false,
         };
-        let response: HttpYouDoSudokuResponse = client.post("https://youdosudoku.com/api")
+        let response: HttpYouDoSudokuResponse = client
+            .post("https://youdosudoku.com/api")
             .json(&request_body)
-            .send().unwrap().json().unwrap();
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
 
         //println!("{:#?}", response);
 
@@ -48,7 +54,9 @@ impl SudokuSolverApp {
         for (i, c) in response.puzzle.chars().enumerate() {
             let row = i / 9 as usize;
             let col = i % 9 as usize;
-            self.puzzle[row][col] = c as usize - '0' as usize;
+            let digit = c as usize - '0' as usize;
+            self.puzzle[row][col] = digit;
+            self.potentials[row][col] = if digit>0 { 1 << (digit - 1) } else {0b111111111};
         }
         for (i, c) in response.solution.chars().enumerate() {
             let row = i / 9 as usize;
@@ -69,46 +77,108 @@ impl SudokuSolverApp {
                 let col = i % 9 as usize;
                 let cell_row = row / 3 as usize;
                 let cell_col = col / 3 as usize;
-                println!("element {i} cell row {cell_row} cell col {cell_col}");
-                
+                //println!("element {i} cell row {cell_row} cell col {cell_col}");
+
                 if puzzle[row][col] == 0 {
                     loop {
-                        //thread::sleep(std::time::Duration::from_millis(20));
-                        let mut b = {
-                            board.lock().unwrap()
-                        };
+                        thread::sleep(std::time::Duration::from_millis(200));
+                        let mut b = { board.lock().unwrap() };
                         b[row][col] += 1;
                         let digit = b[row][col];
-                        println!("{digit}");
+                        //println!("{digit}");
                         if digit == 10 {
                             b[row][col] = 0;
                             i -= 1;
                             dir = -1;
                             break;
                         }
-                        let col_check = b.iter().map(|r| r[col] ).filter(|&e| e == b[row][col]).collect::<Vec<_>>().len();
-                        let row_check = b[row].iter().filter(|&e| e == &b[row][col]).collect::<Vec<_>>().len();
-                        let cr = cell_row*3;
-                        let cc = cell_col*3;
-                        let cell_check = b[cr..cr+3].iter().flat_map(|r| r[cc..cc+3].iter().collect::<Vec<_>>()).filter(|&e| e == &b[row][col]).collect::<Vec<_>>().len();
-                        println!("COL {:?} {}", b.iter().map(|r| r[col]).collect::<Vec<_>>(), col_check);
-                        println!("ROW {:?} {}", b[row], row_check);
-                        println!("CELL {:?} {}", b[cr..cr+3].iter().flat_map(|r| r[cc..cc+3].iter().collect::<Vec<_>>()).collect::<Vec<_>>(), cell_check);
+                        let col_check = b
+                            .iter()
+                            .map(|r| r[col])
+                            .filter(|&e| e == b[row][col])
+                            .collect::<Vec<_>>()
+                            .len();
+                        let row_check = b[row]
+                            .iter()
+                            .filter(|&e| e == &b[row][col])
+                            .collect::<Vec<_>>()
+                            .len();
+                        let cr = cell_row * 3;
+                        let cc = cell_col * 3;
+                        let cell_check = b[cr..cr + 3]
+                            .iter()
+                            .flat_map(|r| r[cc..cc + 3].iter().collect::<Vec<_>>())
+                            .filter(|&e| e == &b[row][col])
+                            .collect::<Vec<_>>()
+                            .len();
+                        //println!("COL {:?} {}", b.iter().map(|r| r[col]).collect::<Vec<_>>(), col_check);
+                        //println!("ROW {:?} {}", b[row], row_check);
+                        //println!("CELL {:?} {}", b[cr..cr+3].iter().flat_map(|r| r[cc..cc+3].iter().collect::<Vec<_>>()).collect::<Vec<_>>(), cell_check);
 
-
-                        if row_check == 1 && col_check == 1 && cell_check == 1{
+                        if row_check == 1 && col_check == 1 && cell_check == 1 {
                             i += 1;
                             dir = 1;
                             break;
                         }
                     }
                 } else {
-                    if i == 0 { dir = 1; }
-                    if dir == 1 { i += 1; } else if dir == -1 { i -= 1; }
+                    if i == 0 {
+                        dir = 1;
+                    }
+                    if dir == 1 {
+                        i += 1;
+                    } else if dir == -1 {
+                        i -= 1;
+                    }
                 }
             }
             println!("Solved?");
         });
+    }
+    fn update_potentials(&mut self) {
+        let mut i = 0;
+        while i < 81 {
+            let row = i / 9 as usize;
+            let col = i % 9 as usize;
+            let cell_row = row / 3 as usize;
+            let cell_col = col / 3 as usize;
+            //println!("element {i} cell row {cell_row} cell col {cell_col}");
+            let board = { self.board.lock().unwrap() };
+
+            if board[row][col] == 0 {
+                let col_check = board
+                    .iter()
+                    .map(|r| r[col])
+                    .filter(|&e| e != 0)
+                    .collect::<Vec<_>>();
+                    
+                //println!("{col_check:?}");
+                for num in col_check {
+                    self.potentials[row][col] &= !( 1 << (num-1));
+                }
+                let row_check = board[row]
+                    .iter()
+                    .filter(|&e| *e != 0)
+                    .collect::<Vec<_>>();
+                //println!("{row_check:?}");
+                for num in row_check {
+                    self.potentials[row][col] &= !( 1 << (num-1));
+                }
+                let cr = cell_row * 3;
+                let cc = cell_col * 3;
+                let cell_check = board[cr..cr + 3]
+                    .iter()
+                    .flat_map(|r| r[cc..cc + 3].iter().collect::<Vec<_>>())
+                    .filter(|&e| *e != 0)
+                    .collect::<Vec<_>>();
+                //println!("{cell_check:?}");
+                for num in cell_check {
+                    self.potentials[row][col] &= !( 1 << (num-1));
+                }
+            }
+            i+=1;
+        }
+        println!("Updated Potentials");
     }
 }
 
@@ -200,6 +270,11 @@ impl eframe::App for SudokuSolverApp {
             if ui.button("Brute Force").clicked() {
                 self.brute_force(&ctx);
             }
+            ui.separator();
+            ui.checkbox(&mut self.show_potentials, "Show Potentials");
+            if ui.button("Update Potentials").clicked() {
+                self.update_potentials();
+            }
         });
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
@@ -207,6 +282,7 @@ impl eframe::App for SudokuSolverApp {
                     *self.board.lock().unwrap() = self.puzzle;
                 }
                 ui.label(format!("Difficulty: {}", self.difficulty));
+                ui.add_space(40.0);
                 if *self.board.lock().unwrap() == self.solution && self.solution[0][0] != 0 {
                     ui.label("Won");
                 }
@@ -220,10 +296,28 @@ impl eframe::App for SudokuSolverApp {
                             let digit = self.board.lock().unwrap()[i][j];
                             let puzzle_digit = self.puzzle[i][j];
                             let solution_digit = self.solution[i][j];
+                            let potential = self.potentials[i][j];
                             let cell = egui::Button::new(if digit != 0 {
                                 digit.to_string()
                             } else {
-                                "".to_string()
+                                // TODO: Is there a better way to do this?
+                                if self.show_potentials {
+                                    let fmt = format!(
+                                        "{} {} {}\n{} {} {}\n{} {} {}",
+                                        (potential & (1 << 0) == (1 << 0)) as usize *1,
+                                        (potential & (1 << 1) == (1 << 1)) as usize *2,
+                                        (potential & (1 << 2) == (1 << 2)) as usize *3,
+                                        (potential & (1 << 3) == (1 << 3)) as usize *4,
+                                        (potential & (1 << 4) == (1 << 4)) as usize *5,
+                                        (potential & (1 << 5) == (1 << 5)) as usize *6,
+                                        (potential & (1 << 6) == (1 << 6)) as usize *7,
+                                        (potential & (1 << 7) == (1 << 7)) as usize *8,
+                                        (potential & (1 << 8) == (1 << 8)) as usize *9,
+                                    );
+                                    fmt
+                                } else {
+                                    "".to_string()
+                                }
                             });
                             // Disable the cells that belong in the puzzle
                             ui.add_enabled_ui(puzzle_digit == 0, |ui| {
@@ -243,10 +337,11 @@ impl eframe::App for SudokuSolverApp {
                                     close_on_click_outside,
                                     |ui| {
                                         egui::Grid::new("some_unique_id").show(ui, |ui| {
-                                            for n in 1..10 {
+                                            for n in 0..10 {
                                                 if ui.button(format!("\t{n}\t")).clicked() {
                                                     self.board.lock().unwrap()[i][j] = n;
                                                 }
+                                                if n == 0 { ui.end_row(); continue;}
                                                 if n % 3 == 0 {
                                                     ui.end_row();
                                                 }
@@ -255,13 +350,17 @@ impl eframe::App for SudokuSolverApp {
                                     },
                                 );
                             });
-                            if (j+1) % 3 == 0 && j != 8 { ui.separator();}
+                            if (j + 1) % 3 == 0 && j != 8 {
+                                ui.separator();
+                            }
                         }
                     });
                     // The length of this separator is extending longer than the cells.
-                    // Temporarily set a size for the separator and guess the length ratio 
+                    // Temporarily set a size for the separator and guess the length ratio
                     // it should be
-                    if (i+1) % 3 == 0 && i != 8 { ui.add_sized([cell_size*10.85, 0.5], egui::Separator::default());}
+                    if (i + 1) % 3 == 0 && i != 8 {
+                        ui.add_sized([cell_size * 10.85, 0.5], egui::Separator::default());
+                    }
                 }
             });
         });
